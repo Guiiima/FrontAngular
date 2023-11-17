fbe38d707a108306f6eb1a12b9f1082c00c9b73a
LuanPozzobon <108753073+LuanPozzobon@users.noreply.github.com>
2023-11-17 19:40:46 -0300

    Study Room

---
+++ src/app/study-room-controller/StudyRoom.ts
@@ -0,0 +1,5 @@
+export class StudyRoom {
+    roomNumber: number = 0;
+    capacity: number = 0;
+    isAvailable: boolean = true;
+}
--- src/app/study-room-controller/study-room-controller.component.ts
+++ src/app/study-room-controller/study-room-controller.component.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { StudyRoomService } from './study-room.service';
+import { StudyRoom } from './StudyRoom';
+import { CdkTableDataSourceInput } from '@angular/cdk/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-    selector: 'app-study-room-controller',
-    standalone: true,
-    templateUrl: './study-room-controller.component.html',
-    styleUrl: './study-room-controller.component.css',
-    imports: [CommonModule, NavBarComponent]
+  selector: 'app-study-room-controller',
+  standalone: true,
+  imports: [CommonModule, NavBarComponent, MatTableModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './study-room-controller.component.html',
+  styleUrl: './study-room-controller.component.css'
 })
-export class StudyRoomControllerComponent {
+export class StudyRoomControllerComponent implements OnInit {
+  displayedColumns: string[] = ['roomNumber', 'capacity', 'isAvailable', 'occupation'];
+  dataSource: CdkTableDataSourceInput<StudyRoom> = new MatTableDataSource<StudyRoom>();
+  showForm: boolean = false;
 
+  constructor(private studyRoomService: StudyRoomService) { }
+  ngOnInit(): void {
+    this.loadAllRooms();
+  }
+
+  changeOccupation(item: StudyRoom): void{
+    this.studyRoomService.changeOccupation(item.roomNumber).subscribe(resp => {
+      this.loadAllRooms();
+    });
+    
+    console.log(item)
+  }
+
+  searchByCapacity(capacityInput: string): void {
+    event?.preventDefault();
+    var capacity = Number(capacityInput)
+    this.studyRoomService.findRoomsByCapacity(capacity).subscribe(rooms => {
+      
+      this.dataSource = new MatTableDataSource<StudyRoom>(rooms)
+    });
+  }
+
+  openForm(): void {
+    this.showForm = true;
+  }
+
+  newRoom(roomNumber: string, capacity: string): void {   
+    var room = new StudyRoom();
+    if (isNaN(room.roomNumber = Number(roomNumber))) {
+      alert("O campo 'Nº da Sala' deve conter um valor numérico");
+      return;
+    }
+    if (isNaN(room.capacity = Number(capacity))) {
+      alert("O campo 'Capacidade' deve conter um valor numérico");
+      return;
+    }
+    room.isAvailable = true;
+
+    this.studyRoomService.newRoom(room).subscribe();
+    this.loadAllRooms();
+  }
+
+  private loadAllRooms(): void {
+    this.studyRoomService.allRooms().subscribe(rooms =>{
+      this.dataSource = new MatTableDataSource<StudyRoom>(rooms);
+    })
+  }
 }
