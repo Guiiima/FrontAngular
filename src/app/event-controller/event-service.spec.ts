942c396eacd3897cf7800c87d79cefec83389a86
LuanPozzobon <108753073+LuanPozzobon@users.noreply.github.com>
2023-11-19 17:02:10 -0300

    Event2

--- src/app/event-controller/event-service.spec.ts
+++ src/app/event-controller/event-service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { EventService } from './event.service';
 
-describe('ReservationService', () => {
+describe('EventService', () => {
   let service: EventService;
 
   beforeEach(() => {
