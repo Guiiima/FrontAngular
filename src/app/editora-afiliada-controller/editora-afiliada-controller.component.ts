69436281b5961aae139729d2b57f7e9f08987ac7
Gui <guilhermeh000@hotmail.com>
2023-11-19 22:30:01 -0300

    corracao emprestimo

--- src/app/editora-afiliada-controller/editora-afiliada-controller.component.ts
+++ src/app/editora-afiliada-controller/editora-afiliada-controller.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { EditoraAfiliadaService } from './editora-afiliada.service';
 import { EditoraAfiliada } from './EditoraAfiliada';
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { CdkTableDataSourceInput } from '@angular/cdk/table';
 
@@ -13,24 +13,24 @@ import { CdkTableDataSourceInput } from '@angular/cdk/table';
     templateUrl: './editora-afiliada-controller.component.html',
     styleUrl: './editora-afiliada-controller.component.css',
     imports: [CommonModule, NavBarComponent, MatTableModule]
-    
+
 })
-export class EmprestimoControllerComponent {
+export class EditoraAfiliadaControllerComponent {
     displayedColumns: string[] = ['id', 'contato', 'autores', 'qtdLivrosPublicados'];
     dataSource: CdkTableDataSourceInput<EditoraAfiliada> = new MatTableDataSource<EditoraAfiliada>();
     showForm: boolean = false;
-  
+
     constructor(private editoraAfiliadaService: EditoraAfiliadaService){}
     ngOnInit(){
       this.ObterLista();
     }
-  
-  
+
+
     async ObterLista(){
       await this.editoraAfiliadaService.listar().subscribe(editoras => {
         this.dataSource = new MatTableDataSource<EditoraAfiliada>(editoras);
       })
     }
 
-    
+
 }
